baf5f6519ce41793192a7dedd88404dcca0e1e5c
Dimka <semenov_dmitry@hotmail.com>
2025-04-21 17:37:26 +0200

    Milestone1: Country.java enum implemented

---
+++ src/main/java/com/rviewer/skeletons/domain/blockchain/Country.java
@@ -0,0 +1,17 @@
+package com.rviewer.skeletons.domain.blockchain;
+
+public enum Country {
+    BE, EL, LT, PT, BG, ES, LU, RO, CZ,
+    FR, HU, SI, DK, HR, MT, SK, DE, IT,
+    NL, FI, EE, CY, AT, SE, IE, LV, PL;
+
+    public static boolean isValid(String code) {
+        try {
+            Country.valueOf(code);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
+
