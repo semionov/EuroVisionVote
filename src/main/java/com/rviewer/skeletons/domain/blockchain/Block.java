4d2d193e81cfac6cca65516ea0f5bd33115a3bd5
Dimka <semenov_dmitry@hotmail.com>
2025-04-21 14:50:40 +0200

    skeleton: implement Block class structure

---
+++ src/main/java/com/rviewer/skeletons/domain/blockchain/Block.java
@@ -0,0 +1,40 @@
+package com.rviewer.skeletons.domain.blockchain;
+
+public class Block {
+    private String hash;
+    private String previousHash;
+    private long timestamp;
+    private Object data;    // Vote or other data type
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
